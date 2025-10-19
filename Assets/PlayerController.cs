eaa4edc651d80b1ca2f3b50daefd587a5c9407fa
mariogithub <bagyinszki.mario1@gmail.com>
2025-10-19 18:16:41 +0100

    coded

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,65 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigidBody;
+    public float speed = 5.0f;
+    public float jumpForce = 8.0f;
+    public float airControlForce = 10.0f;
+    public float airControlMax = 1.5f;
+    public bool grounded;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // You can add input checks here if needed
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        // check if we are on the ground
+        if (grounded)
+        {
+            if (Input.GetAxis("Jump") > 0.0f)
+                rigidBody.AddForce(new Vector2(0.0f, jumpForce), ForceMode2D.Impulse);
+            else
+                rigidBody.linearVelocity = new Vector2(speed * h, rigidBody.linearVelocityY);
+        }
+        else
+        {
+            // allow a small amount of movement in the air
+            float vx = rigidBody.linearVelocityX;
+            if (h * vx < airControlMax)
+                rigidBody.AddForce(new Vector2(h * airControlForce, 0));
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 3)
+        {
+            grounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 3)
+        {
+            grounded = false;
+        }
+    }
+
+}
+
+
+
+
+
